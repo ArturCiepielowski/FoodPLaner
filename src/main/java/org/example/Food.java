de0e9899c10385ca80b42583435ccc9d9501c3c4
Artur Ciepielowski <artciepi@gmail.com>
2024-07-01 22:20:00 +0200

    Expanded food map, ingredients list

--- src/main/java/org/example/Food.java
+++ src/main/java/org/example/Food.java
@@ -1,24 +1,26 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class Food {
-    private static final HashMap<Integer, String> FOOD_MAP = new HashMap<>();
+    private static final HashMap<String, String[]> FOOD_MAP = new HashMap<>();
     private static final HashMap<String, String> DAYS_LIST = new HashMap<>();
 
     static {
-        FOOD_MAP.put(1, "Kotlety drobiowe");
-        FOOD_MAP.put(2, "Kotlety mielone");
-        FOOD_MAP.put(3, "Spagetti Neapoli");
-        FOOD_MAP.put(4, "Spagetti Bolognese");
-        FOOD_MAP.put(5, "Chili con carne");
-        FOOD_MAP.put(6, "Naleśniki");
-        FOOD_MAP.put(7, "Pancake");
-        FOOD_MAP.put(8, "Pizza");
-        FOOD_MAP.put(9, "Pulpety");
-        FOOD_MAP.put(10, "Kurczak w sosie pieczeniowym");
+        FOOD_MAP.put("Kotlety drobiowe", new String[]{"Pierś Z Kurczaka", "Przyprawa do mięs","Mąka tortowa","Bułka tarta","4xJajka","Sól","Pieprz","Olej"});
+        FOOD_MAP.put("Kotlety mielone",new String[]{"Mięso mielone","Czerstwa bułka","Cebula","1xJajko","Sól","Woda/mleko"});
+        FOOD_MAP.put("Spagetti Neapoli",new String[]{"Makaron","Sos Neapoli","Mozarella","Groszek","Olej","Woda"});
+        FOOD_MAP.put("Spagetti Bolognese",new String[]{"Makaron","Sos Bolognese","Mozarella","Czerwona fasola","Olej","Woda"});
+        FOOD_MAP.put("Chili con carne",new String[]{"placeholder","placeholder"});
+        FOOD_MAP.put("Naleśniki",new String[]{"placeholder1","placeholder1"});
+        FOOD_MAP.put("Pancake",new String[]{"placeholder2","placeholder2"});
+        FOOD_MAP.put("Pizza",new String[]{"placeholder3","placeholder3"});
+        FOOD_MAP.put("Pulpety",new String[]{"placeholder4","placeholder4"});
+        FOOD_MAP.put("Kurczak w sosie pieczeniowym",new String[]{"placeholder5","placeholder5"});
 
         DAYS_LIST.put("MONDAY", "Poniedziałek");
         DAYS_LIST.put("TUESDAY", "Wtorek");
@@ -29,22 +31,26 @@ public class Food {
         DAYS_LIST.put("SUNDAY", "Niedziela");
     }
 
-    private String getRandomFood() {
-        Random randomFood = new Random();
-        return FOOD_MAP.get(randomFood.nextInt(10) + 1);
-    }
-
-    private String getTodayName(LocalDate nextDays) {
-        String dayOfWeek =nextDays.getDayOfWeek().toString();
-        return DAYS_LIST.get(dayOfWeek);
-    }
-
     public Food(int amountOfDays) {
         LocalDate today = LocalDate.now();
         for (int dayOfWeek = 0; dayOfWeek < amountOfDays; dayOfWeek++) {
             LocalDate nextDays = today.plusDays(dayOfWeek);
-            System.out.println(nextDays+" "+getTodayName(nextDays)+": "+getRandomFood());
+            String chosenFood = getRandomFood();
+            System.out.println(nextDays + " " + getTodayName(nextDays) + ": " + chosenFood);
+            //todo stworzyc dodatkowy czat pytajacy sie czy wypisac liste zakupow
         }
+    }
+
+    private String getRandomFood() {
+        List<String> dishList = new ArrayList<>(FOOD_MAP.keySet());
+        Random randomFood = new Random();
+        int randomIndex = randomFood.nextInt(dishList.size());
+        return dishList.get(randomIndex);
+    }
+
+    private String getTodayName(LocalDate nextDays) {
+        String dayOfWeek = nextDays.getDayOfWeek().toString();
+        return DAYS_LIST.get(dayOfWeek);
     }
 
 }
